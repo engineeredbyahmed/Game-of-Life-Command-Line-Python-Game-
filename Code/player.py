b161ec8bf9f6a42b527a8807d026e28e69d5ca6b
Ahmed <ahmed@MacBook-Air-byan.local>
2026-02-24 00:29:44 +0000

    player class updated

---
+++ Code/player.py
@@ -0,0 +1,23 @@
+class Player: 
+
+    """
+    This class contains players, their attributes, and methods. 
+    
+    """
+
+    def __init__(self, name):
+        self.name = name
+        self.cash = 5000
+        self.position = 0
+        self.salary = 0 
+        self.retired = False 
+
+    def earn(self, amount):
+        self.cash += amount
+
+    def pay(self, amount):
+        self.cash -= amount
+
+
+
+
